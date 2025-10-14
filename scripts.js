003aa298ab6f64d2340a580e6a600ffafc87ed80
lucasmarios26 <132863916+lucasmarios26@users.noreply.github.com>
2025-10-14 20:13:16 -0300

    6 - Estilização e botao carregar imagem no JS

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const upLoadBtn = document.getElementById('upload-btn');
+const imageUpload = document.getElementById('image-upload');
+
+upLoadBtn.addEventListener('click', () => {
+    imageUpload.click();
+});
