745a22c45a05ccbb3fbb40fd32f9d7c360e9ba53
lucasmarios26 <132863916+lucasmarios26@users.noreply.github.com>
2026-01-14 18:36:24 -0300

    7 - Function lerConteudoDoArquivo

--- scripts.js
+++ scripts.js
@@ -4,3 +4,16 @@ const imageUpload = document.getElementById('image-upload');
 upLoadBtn.addEventListener('click', () => {
     imageUpload.click();
 });
+
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({url: leitor.result, nome: arquivo.name});
+        }
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo: ${arquivo.name}`);
+        }
+        leitor.readAsDataURL(arquivo);
+    });
+}7
